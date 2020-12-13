b717177c9a2588495a5d60cea49983f746b936fa
Ravi gajanan gaygol <71166771+Ravigaygol@users.noreply.github.com>
2020-12-13 10:34:46 +0530

    Add files via upload

---
+++ SpringBootExceptionDemo/src/main/java/com/ExceptionMain/SpringBootExceptionDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ExceptionMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExceptionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExceptionDemoApplication.class, args);
+		System.out.println("Exception successful....!!");
+	}
+
+}
---
+++ SpringBootExceptionDemo/src/test/java/com/ExceptionMain/SpringBootExceptionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExceptionMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
